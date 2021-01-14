ee1a0bc95f11c3c417d94306909c9eba55b5f8ad
Gabryss <gabriel.garcia@utbm.fr>
2021-01-14 22:26:36 +0100

    Commit for Surface laptop (Saving)

--- src/components/HomeLoged/HomeLoged.js
+++ src/components/HomeLoged/HomeLoged.js
@@ -1,3 +1,4 @@
+import axios from "axios"
 
 export default {
   name: 'home-loged',
@@ -5,7 +6,6 @@ export default {
   props: [],
   data () {
     return {
-      users
 
     }
   },
@@ -13,11 +13,28 @@ export default {
 
   },
   mounted () {
+    this.GetProfiles()
+    
 
   },
   methods: {
+    GetProfiles: function(){
+      axios.get('http://127.0.0.1:8000/api/community/discipline/',
+                {
+                  headers: {
+                  'Authorization': `Bearer ${localStorage.token}`
+                }
+                },
+                {"username":this.username, "password":this.password})
+      .then(response => {
+        console.log(response)
 
+      })
+      .catch(error => {
+        console.log(error.response) 
+        console.log(error.response.data) 
+        this.errors = error.response.data.detail
+      })
+    }
   }
 }
-
-
--- src/components/HomeLoged/index.vue
+++ src/components/HomeLoged/index.vue
@@ -1,4 +1,3 @@
 <template src="./HomeLoged.html"></template>
 <script src="./HomeLoged.js"></script>
 <style src="./HomeLoged.css" scoped lang="css"></style>
-
