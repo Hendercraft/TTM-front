4ff8419fa51b054fbcf6a3b60184ee9db6e7b47b
Gabryss <zeratul312@gmail.com>
2021-03-25 18:00:37 +0100

    Import new upload file component

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <template>
   <div id="app">
+    <link type="text/css" rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" />
     <router-view/>
+    <upload-files></upload-files>
   </div>
 </template>
 
 <script>
+import UploadFiles from './components/UploadFiles.vue'
 export default {
+  components: { UploadFiles },
   name: 'App'
 }
 </script>
