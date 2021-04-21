d919cf0484b158547e6bfecd86d6f26f78140b61
Gabryss <gabriel.garcia@utbm.fr>
2021-04-21 14:55:28 +0200

    Include Bar and login/register component

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,21 @@
 <template>
   <div id="app">
+    <nav id="upper-navbar">
+      <ul>
+        <li class="upper-nabar-elements"><router-link to="/login">Connexion</router-link></li>
+        <li class="upper-nabar-elements"><router-link to="/register">Inscription</router-link></li>
+      </ul>
+    </nav>
+    <bar class="sidenav"></bar>
     <link type="text/css" rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" />
     <router-view/>
   </div>
 </template>
 
 <script>
+import Bar from '@/components/Bar'
 export default {
-  components: { },
+  components: { Bar },
   name: 'App'
 }
 </script>
@@ -20,5 +28,44 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  margin-left: 10%; /* Same as the width of the sidebar */
+  padding: 0px 10px;
+}
+
+/* The sidebar menu */
+.sidenav {
+    height: 100%; /* Full-height: remove this if you want "auto" height */
+    width: 10%; /* Set the width of the sidebar */
+    position: fixed; /* Fixed Sidebar (stay in place on scroll) */
+    z-index: 1; /* Stay on top */
+    top: 0; /* Stay at the top */
+    left: 0;
+    background-color: #111; /* Black */
+    overflow-x: hidden; /* Disable horizontal scroll */
+    padding-top: 20px;
+  }
+.upper-nabar-elements {
+    margin: 0;
+    padding: 0;
+    list-style-type: none;
+}
+ul{
+  display: block;
+  float: right;
+}
+li a {
+  /* display: block; */
+  width: 100px;
+  color: #000;
+  padding: 8px 16px;
+  text-decoration: none;
+}
+li a:hover {
+  background-color: rgba(117, 117, 117, 0.623);
+  color: white;
+}
+.active {
+  background-color: #4CAF50;
+  color: white;
 }
 </style>
