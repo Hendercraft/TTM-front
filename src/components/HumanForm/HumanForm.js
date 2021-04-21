9b73d9593dba691c62fa54cb7c21e6e5429570f6
Gabryss <gabriel.garcia@utbm.fr>
2021-04-21 23:03:40 +0200

    update for vue dynamic

--- src/components/HumanForm/HumanForm.js
+++ src/components/HumanForm/HumanForm.js
@@ -1,4 +1,6 @@
 import UploadFiles from '../UploadFiles.vue'
+import UploadService from '../../services/UploadFilesService'
+import http from '../../http-common'
 
 export default {
   name: 'human-form',
@@ -10,7 +12,31 @@ export default {
       format:null,
       formatGroup:null,
       fileId: null,
-      output: ""
+      output: "",
+
+      designation: "",
+      repere_historique:"",
+      date: null,
+      localization: "",
+      adresse:"",
+      description: "",
+      mots_cles: "",
+      source: "",
+      editeur: "",
+      droits: "",
+      ressource_type: "",
+      immatriculation: "",
+      etude: "",
+      auteur: "",
+      date_etude: "",
+      ressource_file: "",
+
+      selectedFiles: undefined,
+      currentFile: undefined,
+      progress: 0,
+      message: '',
+      fileId: null,
+      fileInfos: [],
     }
   },
   computed: {
@@ -36,12 +62,59 @@ export default {
       
       this.output = '<p>Your Selected Group is <strong>' + this.formatGroup +'</strong></p>';
       console.log(this.formatGroup);
-    }
+    },
 
+  
+  selectFile () {
+    this.selectedFiles = this.$refs.file.files
   },
-  getDataFromFileHandler: function(value){
-    this.fileId = value
-  }
-}
+  upload () {
+    this.progress = 0
+    console.log('uploading...')
+    this.currentFile = this.selectedFiles.item(0)
+    UploadService.upload(this.currentFile, event => {
+      this.progress = Math.round((100 * event.loaded) / event.total)
+      }, this.formatGroup)
+      .then(response => {
+        console.log("Uploaded")
+        this.message = response.data
+        this.fileId = response.data.id
+        http.post('database/ressource/create/', {
+          "designation": this.designation,
+          "repere_historique": this.repere_historique,
+          "ressource_date": this.date,
+          "localization": this.localization,
+          "adress": this.adresse,
+          "description": this.description,
+          "mots_cles": this.mots_cles,
+          "ressource_source": this.source,
+          "editeur": this.editeur,
+          "droits": this.droits,
+          "ressource_type": this.ressource_type,
+          "ressource_format": this.format,
+          "immatriculation": this.immatriculation,
+          "etude": this.etude,
+          "auteur": this.auteur,
+          "date_etude": this.date_etude,
+          "ressource_file": this.fileId
+        })
+        .then(response =>{
+          console.log(response.data)
+        })
+        // this.$emit(this.fileId)
+        // console.log(this.fileId)
+      })
+      .then(files => {
+        this.fileInfos = files.data.results
+      })
+      .catch(() => {
+        this.progress = 0
+        this.message = 'Could not upload the file!'
+        this.currentFile = undefined
+      })
 
+    this.selectedFiles = undefined
+    }
+  },
+}
 
