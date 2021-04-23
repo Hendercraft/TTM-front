53f733e2fc3121fba24b490a64c2dd56f32dfb1c
Gabryss <gabriel.garcia@utbm.fr>
2021-04-23 19:04:34 +0200

    Add sub-menu and changes apperances of the home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -8,7 +8,9 @@ export default {
   props: [],
   data () {
     return {
-
+      visite_virtuelle:false,
+      multimedia:false,
+      cartographie:true,
     }
   },
   computed: {
@@ -18,6 +20,20 @@ export default {
 
   },
   methods: {
-
+    changeToVisite_virtuelle(){
+      this.multimedia = false
+      this.cartographie = false
+      this.visite_virtuelle = true
+    },
+    changeToMultimedia(){
+      this.multimedia = true
+      this.cartographie = false
+      this.visite_virtuelle = false
+    },
+    changeToCartographie(){
+      this.multimedia = false
+      this.cartographie = true
+      this.visite_virtuelle = false
+    }
   }
 }
