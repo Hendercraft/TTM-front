b976c88c577ac52dac5caa7d7fc53e282e825360
Gabryss <gabriel.garcia@utbm.fr>
2021-05-04 23:13:08 +0200

    Add testimony component

---
+++ src/components/testimony/index.vue
@@ -0,0 +1,3 @@
+<template src="./testimony.html"></template>
+<script src="./testimony.js"></script>
+<style src="./testimony.css" scoped lang="css"></style>
---
+++ src/components/testimony/testimony.js
@@ -0,0 +1,22 @@
+
+export default {
+  name: 'testimony',
+  components: {},
+  props: [],
+  data () {
+    return {
+
+    }
+  },
+  computed: {
+
+  },
+  mounted () {
+
+  },
+  methods: {
+
+  }
+}
+
+
