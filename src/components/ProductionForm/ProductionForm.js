8dd0c9c27ba155a6f1497aaaa1e35a34d441c676
Gabryss <gabriel.garcia@utbm.fr>
2021-05-04 23:19:28 +0200

    Add the entire form

--- src/components/ProductionForm/ProductionForm.js
+++ src/components/ProductionForm/ProductionForm.js
@@ -5,7 +5,44 @@ export default {
   props: [],
   data () {
     return {
-      selected_type:null
+      selected_menu:[],
+      selected_type:null,
+      selected_brand:null,
+
+      brandDescription:null,
+      building_select:null,
+      machine_select:null,
+      periode_select:null,
+      inscript_select:null,
+      energie_select:null,
+      material_select:null,
+      state_select:null,
+
+      addConstr:false,
+      addSource: false,
+      addBrand: false,
+
+
+      date_d:null,
+      date_m:null,
+      date_y:null,
+
+      brands:null,
+      brandName:null,
+      inscript_comp:null,
+      state_comp:null,
+      description:null,
+      source:null,
+      editor:null,
+      droits:null,
+      source_type:null,
+      format:null,
+      immatriculation:null,
+      original_immatriculation:null,
+      author:null,
+      study:null,
+      commentary:null,
+
     }
   },
   computed: {
