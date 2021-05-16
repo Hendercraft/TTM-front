13df786db5ffcca3ef18073787b4e030468769fc
Gabryss <gabriel.garcia@utbm.fr>
2021-05-16 11:14:33 +0200

    Add testimony component and change some few parts

--- src/components/testimony/testimony.js
+++ src/components/testimony/testimony.js
@@ -1,3 +1,4 @@
+import http from '../../http-common'
 
 export default {
   name: 'testimony',
@@ -5,6 +6,8 @@ export default {
   props: [],
   data () {
     return {
+      logged:true,
+      testimony:null,
 
     }
   },
@@ -15,7 +18,21 @@ export default {
 
   },
   methods: {
-
+    postTestimony(){
+      http.post("community/testimony/create/", {"testimony":this.testimony}, {
+        headers: {
+          'Authorization': `Bearer ${localStorage.token}`
+        }
+      })
+      .then(response =>{
+        console.log(response)
+        this.testimony = null
+      })
+      .catch(error => {
+        console.log(error)
+      })
+    }
+    
   }
 }
 
