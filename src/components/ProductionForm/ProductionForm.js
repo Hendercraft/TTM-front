f32985b73b2102e45368ef4a63d17a6f8433d169
Gabryss <gabriel.garcia@utbm.fr>
2021-05-20 23:28:51 +0200

    Fix wrapper freeze, pretty textarea, update source fields

--- src/components/ProductionForm/ProductionForm.js
+++ src/components/ProductionForm/ProductionForm.js
@@ -43,6 +43,16 @@ export default {
       study:null,
       commentary:null,
 
+      output:"",
+      ressource_file: "",
+
+      selectedFiles: undefined,
+      currentFile: undefined,
+      progress: 0,
+      message: '',
+      fileId: null,
+      fileInfos: [],
+
     }
   },
   computed: {
@@ -52,6 +62,75 @@ export default {
 
   },
   methods: {
+    getFormatGroup: function(event){
+
+      // 1. Get the selected index
+      const index = event.target.selectedIndex;
+  
+      // 2. Find the selected option
+      const option = event.target.options[index];
+  
+      // 3. Select the parent element (optgroup) for the selected option
+      const optgroup = option.parentElement;
+  
+      // 4. Finally, get the label (Country group)
+      this.formatGroup = optgroup.getAttribute('label');
+      
+      this.output = '<p>Your Selected Group is <strong>' + this.formatGroup +'</strong></p>';
+      console.log(this.formatGroup);
+    },
+
+  
+  selectFile () {
+    this.selectedFiles = this.$refs.file.files
+  },
+  upload () {
+    this.progress = 0
+    console.log('uploading...')
+    this.currentFile = this.selectedFiles.item(0)
+    UploadService.upload(this.currentFile, event => {
+      this.progress = Math.round((100 * event.loaded) / event.total)
+      }, this.formatGroup)
+      .then(response => {
+        console.log("Uploaded")
+        this.message = response.data
+        this.fileId = response.data.id
+        http.post('database/ressource/create/', {
+          "designation": this.designation,
+          "repere_historique": this.repere_historique,
+          "ressource_date": this.date,
+          "localization": this.localization,
+          "adress": this.adresse,
+          "description": this.description,
+          "mots_cles": this.mots_cles,
+          "ressource_source": this.source,
+          "editeur": this.editeur,
+          "droits": this.droits,
+          "ressource_type": this.ressource_type,
+          "ressource_format": this.format,
+          "immatriculation": this.immatriculation,
+          "etude": this.etude,
+          "auteur": this.auteur,
+          "date_etude": this.date_etude,
+          "ressource_file": this.fileId
+        })
+        .then(response =>{
+          console.log(response.data)
+        })
+        // this.$emit(this.fileId)
+        // console.log(this.fileId)
+      })
+      .then(files => {
+        this.fileInfos = files.data.results
+      })
+      .catch(() => {
+        this.progress = 0
+        this.message = 'Could not upload the file!'
+        this.currentFile = undefined
+      })
+
+    this.selectedFiles = undefined
+    },
 
   }
 }
