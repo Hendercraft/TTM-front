9a0c800392ec916f46a4a2a5c0d493803568a5ce
Gabryss <gabriel.garcia@utbm.fr>
2021-05-29 22:33:14 +0200

    Add half responsive

--- src/components/Bar/Bar.js
+++ src/components/Bar/Bar.js
@@ -15,7 +15,15 @@ export default {
 
   },
   methods: {
-
+    hamburger()
+    {
+      var x = document.getElementById("myLinks");
+      if (x.style.display === "block") {
+        x.style.display = "none";
+      } else {
+        x.style.display = "block";
+      }
+    }
   }
 }
 
