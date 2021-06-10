776e9cf0280b10a861141a2ad2ecf50ff618c1c4
Gabryss <gabriel.garcia@utbm.fr>
2021-06-11 00:23:11 +0200

    Add list function, style, route and css

--- src/components/Side-Bar/Architecture/Architecture.js
+++ src/components/Side-Bar/Architecture/Architecture.js
@@ -28,17 +28,15 @@ export default {
           }
         })
           .then(response => {
-            console.log(response.data.results)
             for (var i = 0; i < response.data.results.length; i++)
             {
               this.data = response.data.results[i]
-              console.log(this.data)
               if(this.data['field'] == 'Architecture')
               {
                 this.architectures.push(this.data)
-                console.log(this.architectures)
               }
             }
+            console.log(this.data)
           })
     },
 
