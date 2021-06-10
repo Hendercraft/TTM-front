50fa719f014c4cf5d6aa4797d2fe8178dba8ef4a
Gabryss <gabriel.garcia@utbm.fr>
2021-06-10 19:36:18 +0200

    Add a first sketch of Architecture visualisation

--- src/components/Side-Bar/Architecture/Architecture.js
+++ src/components/Side-Bar/Architecture/Architecture.js
@@ -1,3 +1,4 @@
+import http from "../../../http-common"
 
 export default {
   name: 'architecture',
@@ -5,16 +6,37 @@ export default {
   props: [],
   data () {
     return {
-
+      architectures:[],
     }
   },
   computed: {
 
   },
   mounted () {
-
+    this.getArchitecture()
   },
   methods: {
+    getArchitecture() {
+      http.get("database/ressources/",{
+        headers: {
+          'Content-type': 'application/json',
+          'Authorization': `Bearer ${localStorage.token}`
+        },
+        params: {
+            page:1
+          }
+        })
+          .then(response => {
+            for (const data in response.data.results)
+            {
+              if(data.field == 'Architecture')
+              {
+                this.architectures.push(data)
+                console.log(this.architectures)
+              }
+            }
+          })
+    },
 
   }
 }
