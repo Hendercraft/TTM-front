f897836d228c26659baef6c3d3e91e8e1651cd07
Gabryss <gabriel.garcia@utbm.fr>
2021-06-19 14:48:21 +0200

    Add modals, filter and login verification

--- src/components/Side-Bar/Architecture/Architecture.js
+++ src/components/Side-Bar/Architecture/Architecture.js
@@ -1,12 +1,17 @@
 import http from "../../../http-common"
+import LoginModal from '../../modals/Modal_Login'
+import DeleteRessourceModal from '../../modals/Modal_Delete_Ressource'
+import { tokenIsValid } from "../../../services/jwtService"
 
 export default {
   name: 'architecture',
-  components: {},
+  components: {LoginModal, DeleteRessourceModal},
   props: [],
   data () {
     return {
       architectures:[],
+      deleteID:null,
+      playload:null,
       data:null,
     }
   },
@@ -21,7 +26,6 @@ export default {
       http.get("database/ressources/",{
         headers: {
           'Content-type': 'application/json',
-          'Authorization': `Bearer ${localStorage.token}`
         },
         params: {
             page:1
@@ -39,6 +43,47 @@ export default {
             console.log(this.data)
           })
     },
+
+    //Used to display delete-ressource modal and pass deleteID in arg
+    deleteRessource(id)
+    {
+      const result = tokenIsValid()
+      console.log(result)
+      if(result==true)
+      {
+        this.deleteID = id
+        this.$modal.show('modal-delete-ressource')
+      }
+      if(result==false)
+      {
+        this.$modal.show('modal-login')
+      }
+      
+      
+    },
+
+    //Used to remove the supressed ressource from the displayed list
+    deletedConfirmation(playload)
+    {
+      if(this.deleteID != playload.id)
+      {
+        return "error"
+      }
+      else
+      {
+        this.playload = playload
+        this.architectures.forEach(element => {
+          if(element.id==this.playload.id)
+          {
+            const index = this.architectures.indexOf(element)
+            if(index>-1)
+            {
+              this.architectures.splice(index,1)
+            }
+          }
+        })
+      }
+    }
 
   }
 }
