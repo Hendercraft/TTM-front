71e4b238d7cd38c6b340d2a0e8893a375c1d5022
Gabryss <gabriel.garcia@utbm.fr>
2021-06-22 09:54:43 +0200

    Add token Bearer authorization, change the endpoint and add the right file_type name

--- src/services/UploadFilesService.js
+++ src/services/UploadFilesService.js
@@ -5,12 +5,13 @@ class UploadFilesService {
     let formData = new FormData()
 
     formData.append('url', file)
-    formData.append('fileType', type)
+    formData.append('file_type', type)
     formData.append('file_extension', format)
 
-    return http.post('database/files/create/', formData, {
+    return http.post('database/file/create/', formData, {
       headers: {
-        'Content-Type': 'multipart/form-data'
+        'Content-Type': 'multipart/form-data',
+        'Authorization': `Bearer ${localStorage.token}`
       },
       onUploadProgress
     })
