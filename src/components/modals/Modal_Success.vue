fb6684a60643649efb4c0b2372b03d82f9fa08ee
Gabryss <gabriel.garcia@utbm.fr>
2021-06-26 23:22:09 +0200

    Add success modal

---
+++ src/components/modals/Modal_Success.vue
@@ -0,0 +1,40 @@
+<template>
+  <modal name="modal-success" transition="pop-out" :width="modalWidth" :focus-trap="true" :height="300">
+    <div class="box">
+      <div class="partition-title">
+        Vous avez bien ajouter la ressource !
+      </div>
+    </div>
+  </modal>
+</template>
+<script>
+/* eslint-disable */
+const MODAL_WIDTH = 656
+export default {
+  name: 'SuccessModal',
+  data() {
+    return {
+      modalWidth: MODAL_WIDTH,
+      username:null,
+      password:null
+    }
+  },
+  created() {
+    this.modalWidth =
+      window.innerWidth < MODAL_WIDTH ? MODAL_WIDTH / 2 : MODAL_WIDTH
+  },
+  methods: {
+  }
+}
+</script>
+<style lang="css">
+.partition-title{
+  font-size: 30px;
+  text-align: center;
+}
+
+.box{
+  margin: 1em;
+  height: 250px;
+}
+</style>
