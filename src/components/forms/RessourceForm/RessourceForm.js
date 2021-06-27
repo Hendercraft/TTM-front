638cfa17f8ad9d7deffe707a372efef9e302456b
Gabryss <gabriel.garcia@utbm.fr>
2021-06-27 20:42:25 +0200

    Fix the fetch ressource issue

--- src/components/forms/RessourceForm/RessourceForm.js
+++ src/components/forms/RessourceForm/RessourceForm.js
@@ -17,60 +17,64 @@ export default {
       architectures:[],
       productions:[],
       hommes:[],
+
+      errors:null,
     }
   },
   computed: {
 
   },
   mounted () {
-    this.getRessources()
+    this.getAllChildRessources()
   },
   methods: {
-    getRessources()
+    getAllChildRessources()
     {
-      http.get("database/ressources/",{
-        headers: {
-          'Content-type': 'application/json',
-          'Authorization': `Bearer ${localStorage.token}`
-        },
-        params: {
-            page:1
-          }
-        })
-          .then(response => {
-            for (const data in response.data.results)
+      http.get('/database/objects/')
+      .then(response => {
+        if(response.data.results.length > 0)
+        {
+          response.data.results.forEach(element => {
+            if(element.categorie == "Architecture")
             {
-              if(data.field == 'Architecture')
-              {
-                this.architectures.push(data)
-                console.log(this.architectures)
-              }
-              if(data.field == 'Production')
-              {
-                this.productions.push(data)
-                console.log(this.productions)
-              }
-              if(data.field == 'Homme')
-              {
-                this.hommes.push(data)
-                console.log(this.hommes)
-              }
+              this.architectures.push(element)
+            }
+            if(element.categorie == "Production")
+            {
+              this.productions.push(element)
             }
           })
+        }
+      })
+      
+      http.get('/database/actors/')
+      .then(response => {
+        if(response.data.results.length > 0)
+        {
+          response.data.results.forEach(element => {
+            if(element.categorie == "Hommes")
+            {
+              this.hommes.push(element)
+            }
+          })
+        }
+      })
     },
     postForm()
     {
       //Concatenate architecture and production id (both of them are objects)
-      this.objects = this.architectures
-      this.objects = this.objects.concat(this.productions)
+      this.objects = this.object_architecture
+      this.objects = this.objects.concat(this.object_production)
 
 
+      console.log(this.objects, "object")
+      console.log(this.actor)
       http.post("database/ressource/create/",{
         "name":this.name, 
         "description":this.description, 
         "field":this.field, 
         "objects_son":this.objects, 
-        "actors_son":this.hommes
+        "actors_son":this.actor
         },
         {
         headers: {
@@ -79,6 +83,9 @@ export default {
         }})
           .then(response => {
             console.log(response.data.results)
+          })
+          .catch(error => {
+            this.errors = error.message
           })
     }
   }
